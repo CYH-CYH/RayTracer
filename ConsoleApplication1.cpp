1db32d785905fab2ac4e60edd93e14e79c1e555f
CYH-CYH <64456199+CYH-CYH@users.noreply.github.com>
2020-04-28 13:34:32 +0800

    Put a picture on the screen
    利用C++控制台程序写一个.ppm文件。用PS（或者其他程序）,打开这张图片。

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	int ny = 100;
+	int nx = 200;
+	ofstream fOut("outPutImage.ppm");
+	fOut << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i++)
+		{
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99 * r);
+			int ig = int(255.99 * g);
+			int ib = int(255.99 * b);
+			fOut << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+	fOut.close();
+	return 0;
+}
